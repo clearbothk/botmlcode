550a5a981b68ffd9b150eca1f4919f6a0f8bbe63
utkarsh867 <utkarsh867@gmail.com>
2020-06-26 14:04:48 +0530

    Makes an abstraction for the YOLO object detection code

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -1,180 +1,129 @@
-# USAGE
-# python yolo_object_detection.py --input ../example_videos/janie.mp4 --output ../output_videos/yolo_janie.avi --yolo yolo-coco --display 0
-# python yolo_object_detection.py --input ../example_videos/janie.mp4 --output ../output_videos/yolo_janie.avi --yolo yolo-coco --display 0 --use-gpu 1
-
-# import the necessary packages
-from imutils.video import FPS
 import numpy as np
-import argparse
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", type=str, default="",
-	help="path to (optional) input video file")
-ap.add_argument("-o", "--output", type=str, default="",
-	help="path to (optional) output video file")
-ap.add_argument("-d", "--display", type=int, default=1,
-	help="whether or not output frame should be displayed")
-ap.add_argument("-y", "--yolo", required=True,
-	help="base path to YOLO directory")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
-ap.add_argument("-u", "--use-gpu", type=bool, default=0,
-	help="boolean indicating if CUDA GPU should be used")
-args = vars(ap.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "clearbot.names"])
-LABELS = open(labelsPath).read().strip().split("\n")
+class Detector:
+	net = None
+	confidence_threshold = 0
+	nms_threshold = 0
+	LABELS = None
+	ln = None
 
-# initialize a list of colors to represent each possible class label
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
+	def __init__(self, model_path, use_gpu=False, confidence_thres=0.5, nms_thres=0.3):
+		"""
+		Initialise a instance for YOLOv4 object detection.
+		:param model_path: The path of the model relative to the python script
+		:param use_gpu: Whether to use GPU CUDA or not.
+		:param confidence_thres: The confidence threshold of the results (0 to 1).
+		:param nms_thres: The NMS threshold of the results
+		"""
+		self.confidence_threshold = confidence_thres
+		self.nms_threshold = nms_thres
+		# initialize a list of colors to represent each possible class label
+		labels_path = os.path.sep.join([model_path, "clearbot.names"])
+		self.LABELS = open(labels_path).read().strip().split("\n")
 
-# derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join([args["yolo"], "clearbot.weights"])
-configPath = os.path.sep.join([args["yolo"], "clearbot.cfg"])
+		weights_path = os.path.sep.join([model_path, "clearbot_26_06_20.weights"])
+		config_path = os.path.sep.join([model_path, "clearbot.cfg"])
+		print("[INFO] loading YOLO from disk...")
+		self.net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
-print("[INFO] loading YOLO from disk...")
-net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+		if use_gpu:
+			# set CUDA as the preferable backend and target
+			print("[INFO] setting preferable backend and target to CUDA...")
+			self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+			self.net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-# check if we are going to use GPU
-if args["use_gpu"]:
-	# set CUDA as the preferable backend and target
-	print("[INFO] setting preferable backend and target to CUDA...")
-	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+		# determine only the *output* layer names that we need from YOLO
+		self.ln = self.net.getLayerNames()
+		self.ln = [self.ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+	def detect(self, frame):
+		"""
+		Detect the objects in the frame
+		:param frame: Frame that has been captured from the OpenCV video stream
+		:return: dict object with the objects and the bounding boxes
+		"""
 
-# initialize the width and height of the frames in the video file
-W = None
-H = None
-
-# initialize the video stream and pointer to output video file, then
-# start the FPS timer
-print("[INFO] accessing video stream...")
-vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
-writer = None
-fps = FPS().start()
-
-# loop over frames from the video file stream
-while True:
-	# read the next frame from the file
-	(grabbed, frame) = vs.read()
-
-	# if the frame was not grabbed, then we have reached the end
-	# of the stream
-	if not grabbed:
-		break
-
-	# if the frame dimensions are empty, grab them
-	if W is None or H is None:
+		# construct a blob from the input frame and then perform a forward
+		# pass of the YOLO object detector, giving us our bounding boxes
+		# and associated probabilities
 		(H, W) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(
+			frame, 1 / 255.0, (416, 416), swapRB=True, crop=False
+		)
+		self.net.setInput(blob)
+		layer_outputs = self.net.forward(self.ln)
 
-	# construct a blob from the input frame and then perform a forward
-	# pass of the YOLO object detector, giving us our bounding boxes
-	# and associated probabilities
-	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
-		swapRB=True, crop=False)
-	net.setInput(blob)
-	layerOutputs = net.forward(ln)
+		boxes = []
+		confidences = []
+		class_ids = []
 
-	# initialize our lists of detected bounding boxes, confidences,
-	# and class IDs, respectively
-	boxes = []
-	confidences = []
-	classIDs = []
+		for output in layer_outputs:
+			# loop over each of the detections
+			for detection in output:
+				# extract the class ID and confidence (i.e., probability)
+				# of the current object detection
+				scores = detection[5:]
+				class_id = np.argmax(scores)
+				confidence = scores[class_id]
 
-	# loop over each of the layer outputs
-	for output in layerOutputs:
-		# loop over each of the detections
-		for detection in output:
-			# extract the class ID and confidence (i.e., probability)
-			# of the current object detection
-			scores = detection[5:]
-			classID = np.argmax(scores)
-			confidence = scores[classID]
+				# filter out weak predictions by ensuring the detected
+				# probability is greater than the minimum probability
+				if confidence > self.confidence_threshold:
+					# scale the bounding box coordinates back relative to
+					# the size of the image, keeping in mind that YOLO
+					# actually returns the center (x, y)-coordinates of
+					# the bounding box followed by the boxes' width and
+					# height
+					box = detection[0:4] * np.array([W, H, W, H])
+					(centerX, centerY, width, height) = box.astype("int")
 
-			# filter out weak predictions by ensuring the detected
-			# probability is greater than the minimum probability
-			if confidence > args["confidence"]:
-				# scale the bounding box coordinates back relative to
-				# the size of the image, keeping in mind that YOLO
-				# actually returns the center (x, y)-coordinates of
-				# the bounding box followed by the boxes' width and
-				# height
-				box = detection[0:4] * np.array([W, H, W, H])
-				(centerX, centerY, width, height) = box.astype("int")
+					# use the center (x, y)-coordinates to derive the top
+					# and and left corner of the bounding box
+					x = int(centerX - (width / 2))
+					y = int(centerY - (height / 2))
 
-				# use the center (x, y)-coordinates to derive the top
-				# and and left corner of the bounding box
-				x = int(centerX - (width / 2))
-				y = int(centerY - (height / 2))
+					# update our list of bounding box coordinates,
+					# confidences, and class IDs
+					boxes.append([x, y, int(width), int(height)])
+					confidences.append(float(confidence))
+					class_ids.append(class_id)
 
-				# update our list of bounding box coordinates,
-				# confidences, and class IDs
-				boxes.append([x, y, int(width), int(height)])
-				confidences.append(float(confidence))
-				classIDs.append(classID)
+		# Gives the indexes of the boxes that we should use
+		idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence_threshold, self.nms_threshold)
+		result = []
 
-	# apply non-maxima suppression to suppress weak, overlapping
-	# bounding boxes
-	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
-		args["threshold"])
+		# Extract the results from the detector
+		if len(idxs) > 0:
+			# loop over the indexes we are keeping
+			for i in idxs.flatten():
+				# extract the bounding box coordinates
+				(x, y) = (boxes[i][0], boxes[i][1])
+				(w, h) = (boxes[i][2], boxes[i][3])
+				class_id = class_ids[i]
+				label = self.LABELS[class_id]
+				confidence = confidences[i]
+				result.append({
+					"label": label,
+					"confidence": confidence,
+					"bbox": {
+						"x": x,
+						"y": y,
+						"width": w,
+						"height": h
+					}
+				})
+		return result
 
-	# ensure at least one detection exists
-	if len(idxs) > 0:
-		# loop over the indexes we are keeping
-		for i in idxs.flatten():
-			# extract the bounding box coordinates
-			(x, y) = (boxes[i][0], boxes[i][1])
-			(w, h) = (boxes[i][2], boxes[i][3])
 
-			# draw a bounding box rectangle and label on the frame
-			color = [int(c) for c in COLORS[classIDs[i]]]
-			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-			text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-				confidences[i])
-			cv2.putText(frame, text, (x, y - 5),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-
-	# check to see if the output frame should be displayed to our
-	# screen
-	if args["display"] > 0:
-		# show the output frame
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-
-		# if the `q` key was pressed, break from the loop
-		if key == ord("q"):
-			break
-
-	# if an output video file path has been supplied and the video
-	# writer has not been initialized, do so now
-	if args["output"] != "" and writer is None:
-		# initialize our video writer
-		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-		writer = cv2.VideoWriter(args["output"], fourcc, 30,
-			(frame.shape[1], frame.shape[0]), True)
-
-	# if the video writer is not None, write the frame to the output
-	# video file
-	if writer is not None:
-		writer.write(frame)
-
-	# update the FPS counter
-	fps.update()
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# Sample code for using object detection
+# print("[INFO] accessing video stream...")
+# vs = cv2.VideoCapture(0)
+# detector = Detector("model", use_gpu=True)
+# while True:
+# 	(grabbed, frame) = vs.read()
+# 	if not grabbed:
+# 		break
+# 	result = detector.detect(frame)
